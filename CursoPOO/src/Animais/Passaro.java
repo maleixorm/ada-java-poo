77cdc6390e42453fe6911811fe69adbd415aeae1
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-10-26 12:52:11 -0300

    Criando a classe passaro

---
+++ CursoPOO/src/Animais/Passaro.java
@@ -0,0 +1,9 @@
+package Animais;
+
+public class Passaro extends Animal{
+    static int numeroDePassaros;
+
+    public Passaro(String nome, String cor, int altura, double peso, String estadoDeEspirito) {
+        super(nome, cor, altura, peso, estadoDeEspirito);
+    }
+}
