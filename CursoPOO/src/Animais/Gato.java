768e92167ac51428ed2aec9eff39107a44bdce1e
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-10-26 12:52:00 -0300

    Criando a classe gato

---
+++ CursoPOO/src/Animais/Gato.java
@@ -0,0 +1,17 @@
+package Animais;
+
+public class Gato extends Animal {
+
+    static int numeroDeGatos;
+
+    public Gato(String nome, String cor, int altura, double peso, String estadoDeEspirito) {
+        super(nome, cor, altura, peso, estadoDeEspirito);
+    }
+
+    @Override
+    public String toString() {
+        return "Gato{" +
+                "nome='" + nome + '\'' +
+                '}';
+    }
+}
