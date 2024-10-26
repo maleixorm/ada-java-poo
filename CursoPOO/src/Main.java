4c5cbe28a58c493db9f4abea9e60ef67c6140bfc
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-10-26 13:06:53 -0300

    Implementando o método através da herança de classes

--- CursoPOO/src/Main.java
+++ CursoPOO/src/Main.java
@@ -1,38 +1,17 @@
 import Animais.Cachorro;
+import Animais.Gato;
+import Animais.Passaro;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Cachorro cachorro1 = new Cachorro("Mel", "Preto/Amarelo", 55, 36.5, 22, "Nada");
+        Gato gato1 = new Gato("Felix", "Preto", 15, 4.5, "nada");
+        Passaro passaro1 = new Passaro("Piu Piu", "amarelo", 5, 0.5, "nada");
 
-        byte variavelByte = 120;
-        System.out.println(variavelByte);
-
-        int variavelInt = 2147483;
-        System.out.println(variavelInt);
-
-        Cachorro cachorro1 = new Cachorro("Mel", "Preto/Amarelo", 55,36.5, 22, "Nada");
-        System.out.println(cachorro1.getNumeroDeCachorros());
-        Cachorro cachorro2 = new Cachorro("Puppy", "Marrom", 25,6.5, 5, "Nada");
-        System.out.println(cachorro2.getNumeroDeCachorros());
-        System.out.println(cachorro1.getNumeroDeCachorros());
-        Cachorro cachorro3 = new Cachorro("Thor", "Cinza", 35,18.5, 12, "Nada");
-        Cachorro cachorro4 = new Cachorro("Puppy", "Marrom", 25,6.5, 5, "Nada");
-
-        System.out.println(cachorro2.toString());
-        System.out.println(cachorro4.toString());
-
-        System.out.println(cachorro1.getNome());
-        System.out.println(cachorro2.getPeso());
-        System.out.println(cachorro3.getTamanhoDoRabo());
-
-        cachorro1.latir();
-        System.out.println(cachorro1.getNome() + " pegou uma " + cachorro1.pegar());
-
-        System.out.println("O cachorro está " + cachorro1.interagir("carinho"));
-        System.out.println("O cachorro está " + cachorro1.interagir("vai dormir!"));
-        System.out.println("O cachorro está " + cachorro1.interagir("pisar na patinha"));
-        System.out.println("O cachorro está " + cachorro1.interagir("chamar"));
+        cachorro1.soar();
+        gato1.soar();
+        passaro1.soar();
     }
 }
