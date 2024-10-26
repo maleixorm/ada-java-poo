4b5de7afac2eb36d66cdd5c1c1699a317f0b8553
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-10-26 12:51:40 -0300

    Criando a classe de animais

---
+++ CursoPOO/src/Animais/Animal.java
@@ -0,0 +1,29 @@
+package Animais;
+
+public class Animal {
+    protected String nome;
+    protected String cor;
+    protected int altura;
+    protected double peso;
+    protected String estadoDeEspirito;
+
+    public Animal(String nome, String cor, int altura, double peso, String estadoDeEspirito) {
+        this.nome = nome;
+        this.cor = cor;
+        this.altura = altura;
+        this.peso = peso;
+        this.estadoDeEspirito = estadoDeEspirito;
+    }
+
+    protected void comer() {
+
+    }
+
+    protected void dormir() {
+
+    }
+
+    protected void soar() {
+        System.out.println("Cri cri!");
+    }
+}
