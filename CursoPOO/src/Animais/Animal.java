469bc22abe60b0016e29327cbd40c615b8907650
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-10-29 15:20:14 -0300

    Criando os getters and setters da classe Animal

--- CursoPOO/src/Animais/Animal.java
+++ CursoPOO/src/Animais/Animal.java
@@ -15,6 +15,46 @@ public class Animal {
         this.estadoDeEspirito = estadoDeEspirito;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public String getEstadoDeEspirito() {
+        return estadoDeEspirito;
+    }
+
+    public void setEstadoDeEspirito(String estadoDeEspirito) {
+        this.estadoDeEspirito = estadoDeEspirito;
+    }
+
     protected void comer() {
 
     }
